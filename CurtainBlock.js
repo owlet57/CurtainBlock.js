39ae3779436f0c43c40901ab536ebe9014d4365c
owlet57 <me@owlet57.com>
2013-09-16 17:45:35 +1200

    Begin rewriting for easier access

--- CurtainBlock.js
+++ CurtainBlock.js
@@ -1,41 +1,28 @@
 // CurtainBlock.js
 // Written by Jeremy Mills
-// Options for easy access and setup
-
-CurtainBlock = {
-	init: false,
-	options: {
-		persistent: false, // Disable the alerts if you've confirmed it before
-		redir: 'about:blank', // Redirect URL on denial
-		transparency: false //15% transparency if you want it
-	},
-}
-$(document).ready(function () {
-	// Setup!
-	$curtain = $('.CurtainBlock');
-	$alert = $('.CurtainBlockAlert');
-	$confirm = $('.CurtainBlockConfirm');
-	$deny = $('.CurtainBlockDeny');
+var CurtainBlock = new $.fn.init();
+(function($) {
+	// Setup (for easy dev access)
+	var $curtain = $('.CurtainBlock');
+	var $alert = $('.CurtainBlockAlert');
+	var $confirm = $('.CurtainBlockConfirm');
+	var $deny = $('.CurtainBlockDeny');
 	complete = 0;
-	// Core CurtainBlock methods, including the start
-	// Checks that CurtainBlock.js is setup properly
-	if (CurtainBlock.init) {
+	this.init = function (persistent,transparency,redir) {
 		// Checks for localStorage and disables the curtain, if that option is toggled.
-		if (CurtainBlock.options.persistent) {
+		if (persistent) {
 			if (localStorage.getItem("curtainConfirm")) {
 				$curtain.hide();
 				complete = 1;
 			};
 		};
-		// If we don't have the storage, run the CurtainThing.
+		// If we see that stored key, run the Curtain.
 		if (complete === 0) {
-			// Show Everything (The Curtain is on by default.)
 			$confirm.show();
    			$deny.show();
-			$alert.fadeIn(500);
 			// Redirect / hide based on user input
-			if (CurtainBlock.options.transparency) {
-				$curtain.css('background-color','rgba(0,0,0,0.85')
+			if (transparency) {
+				$curtain.css('background-color','rgba(0,0,0,0.85');
 			}
 			$confirm.click(function () {
 				$curtain.fadeOut(750);
@@ -47,17 +34,14 @@ $(document).ready(function () {
 			});
 			$deny.click(function () {
 				// Redir function
-				if (CurtainBlock.options.redir.length === 0) {
+				if (CurtainBlock.options.redir.length > 0) {
 					// Redirect to a blank page
-					window.location.replace("about:blank")
+					window.location.replace(redir)
 				} else {
 					// Redirect to a page the dev specifies in their setup (by putting a string into CurtainBlock.redir)
-					window.location.replace(CurtainBlock.options.redir)
+					window.location.replace("about:blank")
 				}
 			});
 		}
-	} else {
-		$curtain.hide();
-		console.log("The CurtainBlock.js code is here, but you didn't start it!~ Set CurtainBlock.init to true in your HTML to start it!");
 	}
 });
