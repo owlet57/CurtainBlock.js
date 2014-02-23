owlet57/CurtainBlock.js

commit 25aa6a1cda6e592b1af1bd1598cab0bea6909703

Incorporate $() in setup function

CurtainBlock.js
@@ -2,6 +2,7 @@
 // Written by Jessie Mills
 // Setup (for easy dev access)
 function CurtainBlock(persistent,transparency,redir) {
+$(function() {
 	$curtain = $('.CurtainBlock');
 	$alert = $('.CurtainBlockAlert');
 	$confirm = $('.CurtainBlockConfirm');
@@ -36,3 +37,4 @@ function CurtainBlock(persistent,transparency,redir) {
 			});
 		}
 	}
+});

commit 5c582fda3de15376c028cd3713f8a1873a059488

Fixes syntax errors in the uncompressed JS
- FIX: Remove unneeded semicolons
- FIX: Mixed tabs and spaces issue (bug introduced by unknown commit)
- FIX: TypeError caused by line 41 being }); instead of )}; (bug introduced by commit 25aa6a1cda6e592b1af1bd1598cab0bea6909703)
- UPDATE: Readme details

CurtainBlock.js
@@ -13,12 +13,12 @@ $(function() {
 			if (localStorage.getItem("curtainConfirm")) {
 				$curtain.hide();
 				complete = 1;
-			};
-		};
+			}
+		}
 		// If we see that stored key, run the Curtain.
 		if (complete === 0) {
 			$confirm.show();
-   			$deny.show();
+			$deny.show();
 			// Redirect / hide based on user input
 			if (transparency) {
 				$curtain.css('background-color','rgba(0,0,0,0.85');
@@ -29,7 +29,7 @@ $(function() {
 				// If we're persistent, set up the localStorage
 				if (persistent) {
 					localStorage.setItem("curtainConfirm", true);
-				};
+				}
 			});
 			$deny.click(function () {
 				// Redir function
@@ -37,4 +37,4 @@ $(function() {
 			});
 		}
 	}
-});
+)};